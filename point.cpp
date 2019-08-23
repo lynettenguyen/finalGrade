ac26a67d7561f04aedcbce3ee84525d1dc2ee960
lynettenguyen <39711625+lynettenguyen@users.noreply.github.com>
2019-08-22 19:52:14 -0700

    Update point.cpp

--- point.cpp
+++ point.cpp
@@ -7,16 +7,19 @@ Point::Point(double current, double total, double possible)
 	possiblePoints = possible;
 }
 
+//If there are not any enough points, returns an order pair with a bool
+//and the highest percentage the student can receive based on the point balance.
+//Else, the pair returns true and the minimum amount needed to reach the their desired grade
 std::pair<bool, double> Point::pointsNeeded(double want)
 {
 	double available = totalPoints - possiblePoints;               //allows us to find the available left that a student can earn
 	double totalNeed = (totalPoints * want) - currentPoints;       //the minimum amount of points the student needs to reach desire goal
 	std::pair <bool, double> result;
-	if (totalNeed > available)									   //If there are not any enough points, returns an order pair with a bool 
-	{															   //and the highest percentage the student can receive based on the point
-		double maxPoints = (available+currentPoints) / totalPoints;      //balance.
-		result.first = false;									   //Else, the pair returns true and the minimum amount needed to reach the 
-		result.second = maxPoints;								   //their desired grade 
+	if (totalNeed > available)						
+	{									
+		double maxPoints = (available+currentPoints) / totalPoints;     
+		result.first = false;						
+		result.second = maxPoints;					
 		return result;
 	}
 
